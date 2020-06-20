f4b3db9f2596a987e7933dd33530519b275b61a2
Viliars <redkillerpw@mail.ru>
2020-06-20 14:17:49 +0300

    Test Heroku

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,5 @@
-from app import app, db
-from app.models import User
+from app import app
+# from app.models import User
 from flask import request, jsonify
 from app.utils import get_random_data
 
@@ -8,40 +8,19 @@ from app.utils import get_random_data
 def root():
     return "OK"
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    id = request.args.get('id', 1, type=int)
-    username = request.args.get('username', 1, type=str)
-    user = User(id=id, username=username)
-    db.session.add(user)
-    db.session.commit()
-    return "OK"
+# @app.route('/add_user', methods=['POST'])
+# def add_user():
+#     id = request.args.get('id', 1, type=int)
+#     username = request.args.get('username', 1, type=str)
+#     #user = User(id=id, username=username)
+#     # db.session.add(user)
+#     # db.session.commit()
+#     return "OK"
 
 @app.route('/get_user/<id>', methods=['GET'])
 def get_user(id):
     id = int(id)
-    user = User.query.filter_by(id=id).first()
 
-    if user is not None:
-        data = {
-            "id": user.id,
-            "nickname": user.nickname,
-            "description": user.description,
-            "links": {
-                "steam": user.steam,
-                "discord": user.discord,
-                "twitch": user.twitch
-            },
-            "skills": [],
-            "awards": []
-        }
-
-        for skill in user.skills:
-            data["skills"].append({"title": skill.title, "count": skill.count})
-
-        for award in user.awards:
-            data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
-    else:
-        data = get_random_data(id)
+    data = get_random_data(id)
 
     return jsonify(data)
