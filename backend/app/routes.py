1079542e30ff8b74eff265788572fb6fcdd1916e
Viliars <redkillerpw@mail.ru>
2020-06-20 10:47:32 +0300

    Add get_user

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -15,28 +15,56 @@ def add_user():
     db.session.commit()
     return "OK"
 
-@app.route('/get_user', methods=['GET'])
-def get_user():
-    id = request.args.get('id', 1, type=int)
+@app.route('/get_user/<id>', methods=['GET'])
+def get_user(id):
+    id = int(id)
     user = User.query.filter_by(id=id).first()
 
-    data = {
-        "id": user.id,
-        "nickname": user.nickname,
-        "description": user.description,
-        "links": {
-            "steam": user.steam,
-            "discord": user.discord,
-            "twitch": user.twitch
-        },
-        "skills": [],
-        "awards": []
-    }
+    if user is not None:
+        data = {
+            "id": user.id,
+            "nickname": user.nickname,
+            "description": user.description,
+            "links": {
+                "steam": user.steam,
+                "discord": user.discord,
+                "twitch": user.twitch
+            },
+            "skills": [],
+            "awards": []
+        }
 
-    for skill in user.skills:
-        data["skills"].append({"title": skill.title, "count": skill.count})
+        for skill in user.skills:
+            data["skills"].append({"title": skill.title, "count": skill.count})
 
-    for award in user.awards:
-        data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
+        for award in user.awards:
+            data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
+    else:
+        data = {
+            "id": id,
+            "nickname": "Zortan3301",
+            "description": "Dead by daylight",
+            "links": {
+                "steam": "zortan3301",
+                "discord": None,
+                "twitch": "zortan3301"
+            },
+            "skills": [
+                {
+                    "title": "coordinator",
+                    "count": 3,
+                },
+                {
+                    "title": "leader",
+                    "count": 2,
+                }
+            ],
+            "awards": [
+                {
+                    "title": "Ez Game",
+                    "imgSrc": "https://www.ezride.be/wp-content/uploads/2017/11/cropped-Logo_EZRide_yellow_circle.png"
+                }
+            ]
+        }
 
     return jsonify(data)
