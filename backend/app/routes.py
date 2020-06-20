15ea447b54470652cbca0408d661cda9a05f1c38
Viliars <redkillerpw@mail.ru>
2020-06-20 08:59:49 +0300

    Add Data Base for Users

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -19,7 +19,24 @@ def add_user():
 def get_user():
     id = request.args.get('id', 1, type=int)
     user = User.query.filter_by(id=id).first()
-    if user is not None:
-        return f"User - {user.username}"
-    else:
-        return "ERROR"
+
+    data = {
+        "id": user.id,
+        "nickname": user.nickname,
+        "description": user.description,
+        "links": {
+            "steam": user.steam,
+            "discord": user.discord,
+            "twitch": user.twitch
+        },
+        "skills": [],
+        "awards": []
+    }
+
+    for skill in user.skills:
+        data["skills"].append({"title": skill.title, "count": skill.count})
+
+    for award in user.awards:
+        data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
+
+    return jsonify(data)
