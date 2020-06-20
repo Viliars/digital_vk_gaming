d1d535fab79476133a879adf8a24c559388001d5
Viliars <redkillerpw@mail.ru>
2020-06-20 16:08:58 +0300

    Add new root

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,29 +1,33 @@
 from app import app, db
-from app.models import User
+from app.models import User, Game
 from flask import request, jsonify
-from app.utils import get_random_data
+from app.utils import add_random_user
 
 
 @app.route('/')
 def root():
     return "OK"
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    id = request.args.get('id', 1, type=int)
-    username = request.args.get('username', 1, type=str)
-    user = User(id=id, username=username)
-    db.session.add(user)
+@app.route('/add_favorites', methods=['GET'])
+def add_favorites():
+    user_id = request.args.get('user_id', 1, type=int)
+    game_id = request.args.get('game_id', 1, type=int)
+    user = User.query.filter_by(id=user_id).first()
+    game = Game(game_id=game_id, user=user)
+    db.session.add(game)
     db.session.commit()
     return "OK"
+
 
 @app.route('/get_user/<id>', methods=['GET'])
 def get_user(id):
     id = int(id)
     user = User.query.filter_by(id=id).first()
 
-    if user is not None:
-        data = {
+    if user is None:
+        user = add_random_user(id)
+
+    data = {
             "id": user.id,
             "nickname": user.nickname,
             "description": user.description,
@@ -33,15 +37,17 @@ def get_user(id):
                 "twitch": user.twitch
             },
             "skills": [],
-            "awards": []
+            "awards": [],
+            "games": []
         }
 
-        for skill in user.skills:
-            data["skills"].append({"title": skill.title, "count": skill.count})
+    for skill in user.skills:
+        data["skills"].append({"title": skill.title, "count": skill.count})
 
-        for award in user.awards:
-            data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
-    else:
-        data = get_random_data(id)
+    for award in user.awards:
+        data["awards"].append({"title": award.title, "imgSrc": award.imgSrc})
+
+    for game in user.games:
+        data["games"].append(game.game_id)
 
     return jsonify(data)
