897800f5269acf73f7de65b53e751766ee52fa8b
Max Aminov <aminovmaksim@gmail.com>
2020-06-21 13:31:08 +0500

    Updates

---
+++ precache-manifest.0723f1b7fcc8d883275daf1e73ac00ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c932085e2f56efa93b92",
+    "url": "./static/js/main.4a29ab50.chunk.js"
+  },
+  {
+    "revision": "1f5a5269cbe44864d782",
+    "url": "./static/js/2.7cef8b20.chunk.js"
+  },
+  {
+    "revision": "c932085e2f56efa93b92",
+    "url": "./static/css/main.6346cd35.chunk.css"
+  },
+  {
+    "revision": "1f5a5269cbe44864d782",
+    "url": "./static/css/2.d738e17f.chunk.css"
+  },
+  {
+    "revision": "07f1480e0d921fc136d4ad7550465c71",
+    "url": "./index.html"
+  }
+];
